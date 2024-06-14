74b5a1816c2c8e448b33ebff215cd4ce4012aa97
himanshushukla12 <himanshushukla.shukla3@gmail.com>
2024-06-14 15:57:28 +0530

    Fixed code NER

--- Named Entity Recognition/NamedEntityRecognition.py
+++ Named Entity Recognition/NamedEntityRecognition.py
@@ -1,8 +1,8 @@
 import os
-import io # type: ignore
+import io
 from IPython.display import Image, display, HTML
 from PIL import Image
-import base64  # type: ignore
+import base64 
 import gradio as gr
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
@@ -12,10 +12,24 @@ hf_api_key = os.environ['HF_API_KEY']
 from transformers import pipeline
 
 get_completion = pipeline("ner", model="dslim/bert-base-NER")
+def merge_tokens(tokens):
+    merged_tokens = []
+    for token in tokens:
+        if merged_tokens and token['entity'].startswith('I-') and merged_tokens[-1]['entity'].endswith(token['entity'][2:]):
+            # If current token continues the entity of the last one, merge them
+            last_token = merged_tokens[-1]
+            last_token['word'] += token['word'].replace('##', '')
+            last_token['end'] = token['end']
+            last_token['score'] = (last_token['score'] + token['score']) / 2
+        else:
+            # Otherwise, add the token to the list
+            merged_tokens.append(token)
 
+    return merged_tokens
 def ner(input):
     output = get_completion(input)
-    return {"text": input, "entities": output}
+    merged_tokens = merge_tokens(output)
+    return {"text": input, "entities": merged_tokens}
 
 gr.close_all()
 demo = gr.Interface(fn=ner,
@@ -24,6 +38,6 @@ demo = gr.Interface(fn=ner,
                     title="NER with dslim/bert-base-NER",
                     description="Find entities using the `dslim/bert-base-NER` model under the hood!",
                     allow_flagging="never",
-                    #Here we introduce a new tag, examples, easy to use examples for your application
-                    examples=["My name is Andrew and I live in California", "My name is Poli and work at HuggingFace"])
-demo.launch(share=True, server_port=int(os.environ['PORT3']))
+                    examples=["My name is Andrew, I'm building DeeplearningAI and I live in California", "My name is Poli, I live in Vienna and work at HuggingFace"])
+
+demo.launch(share=True, server_port=int(os.environ['PORT4']))
